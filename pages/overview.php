c53643c84ff807c1b1100eb6671d68dd3abdc12f
Nathan Teissier <nmteissier@gmail.com>
2023-01-09 14:41:25 +0100

    logout

--- pages/overview.php
+++ pages/overview.php
@@ -1,5 +1,14 @@
 <?php
+$title="Overview";
+$page_title="Your profile";
+
+require('./database/connectDB.php');
+
+include "./includes/header.php";
+include "./includes/navbar.php";
+?>
 
 
-
+<?php
+include "./includes/footer.php";
 ?>
